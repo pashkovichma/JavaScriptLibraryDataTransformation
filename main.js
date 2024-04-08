41a8337660476bdd46e008a5bd4c4913da357ce1
Masha <masha@MacBook-Air.local>
2024-04-08 22:41:41 +0200

    stringifyValue method

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ function addValues(a, b) {
     throw new Error('Addition not possible for the provided types.');
   }
 };
+
+function stringifyValue(argumentToSrtingify) {
+  if (Array.isArray(argumentToSrtingify) || 
+      typeof argumentToSrtingify === 'object') {
+    return JSON.stringify(argumentToSrtingify);
+  } else {
+    return String(argumentToSrtingify);
+  }
+};
